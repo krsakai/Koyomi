474314610d65e3e6439f1a1eb4bb3ee623bf1bfa
Rob Wilson <robert.wilson@xdesign.com>
2019-08-23 12:05:25 +0100

    Trying to fix right hand line with left semi-circle.

--- Koyomi/KoyomiCell.swift
+++ Koyomi/KoyomiCell.swift
@@ -134,7 +134,7 @@ final class KoyomiCell: UICollectionViewCell {
                 rightSemicircleView.backgroundColor = color
                 
                 // for bug: unnecessary line
-                rightSemicircleView.frame.size.width = bounds.width / 2 + 50
+                leftSemicircleView.frame.size.width = bounds.width / 2 + 1
                 
                 leftSemicircleView.mask(with: .none)
                 rightSemicircleView.mask(with: .right)
